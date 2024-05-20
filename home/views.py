d6b3c32236a5cc554e09982fd6fa46ff65be9c28
SeyedAmiri <amiridev81@gmail.com>
2024-05-20 15:36:00 +0330

    create serializer and model for home app

--- home/views.py
+++ home/views.py
@@ -1,17 +1,30 @@
 from rest_framework.decorators import APIView
 from rest_framework.response import Response
+from .models import Person
+from .serializers import PersonSerializer
 # from rest_framework.decorators import api_view
+
+
+class Home(APIView):
+	def get(self, request):
+		# persons = Person.objects.all()
+		# serialize_data = PersonSerializer(instance=persons, many=True)
+		persons = Person.objects.get(name='amir')
+		serialize_data = PersonSerializer(instance=persons)
+		return Response(data=serialize_data.data)
+
+
+# class Home(APIView):
+# 	def get(self, request):
+# 		name = request.query_params['name']
+# 		return Response({'name': name})
+#
+# 	def post(self, request):
+# 		data = request.data
+# 		return Response(data)
+
 
 
 # @api_view(['GET', 'POST', 'PUT'])
 # def home(request):
 # 	return Response({'name': 'seyed'})
-
-class Home(APIView):
-	def get(self, request):
-		name = request.query_params['name']
-		return Response({'name': name})
-
-	def post(self, request):
-		data = request.data
-		return Response(data)
