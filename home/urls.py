4214e32af0b1f4e594404d25cf5965e64599e95d
SeyedAmiri <amiridev81@gmail.com>
2024-05-25 17:39:22 +0330

    complete post put and delete functions in home view

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,6 @@ app_name = 'home'
 urlpatterns = [
 	path('', views.Home.as_view(), name='home'),  # endpoint
 	path('questions/', views.QuestionView.as_view()),
+	path('questions/<int:pk>/', views.QuestionView.as_view()),
 
 ]
